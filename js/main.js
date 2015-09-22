9ec069e5aadc124108731c31cdb757a2cb8eaa32
Darius Lam <dariuslamikins@gmail.com>
2015-09-22 19:49:26 -0400

    minor changes to particle settings

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
     dotColor: '#1695A3',
     lineColor: '#1695A3',
-    density: 9000,
+    density: 10000,
     particleRadius: 5,
-    curvedLines: true
+    curvedLines: true,
+    parallaxMultiplier: 10
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
